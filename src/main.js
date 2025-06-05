12af5f367c0450df1bbc8080d7c964214cc699e3
Alejandro García <ccmns12@gmail.com>
2025-06-05 12:21:53 +0200

    feat: update package.json and main.js to include concurrently for parallel script execution

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ document.querySelector('#app').innerHTML = `
           <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/>
         </svg>
         <div class="info-tooltip" id="info-tooltip">
-          Tab ID: ${obrAPI.getTabId().slice(-8)}
+          Tab ID: ${obrAPI.getTabId()}
         </div>
       </div>
     </div>
